2384c5c2ec8cf7e4b3a12254cc75b2c1570ddfaa
SancheZ25RUS <sn_elh-92@mail.ru>
2024-02-14 21:37:08 +1000

    init commit

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true,
+  },
+  'extends': 'google',
+  'overrides': [
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module',
+  },
+  'rules': {
+    'max-len': [
+      'off', { 'code': 80 },
+    ],
+    'object-curly-spacing': [
+      'error', 'always',
+    ],
+    'require-jsdoc': 0,
+    'no-unused-vars': 1,
+  },
+};
