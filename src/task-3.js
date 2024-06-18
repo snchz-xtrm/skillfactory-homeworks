aec451fb268cbf5b7a4e7bf3a9c9f603b33b3794
SancheZ25RUS <sn_elh-92@mail.ru>
2024-06-18 17:41:15 +1000

    add: project requirements

--- src/task-3.js
+++
@@ -1,9 +0,0 @@
-// // // // // //
-//  Задание 3  //
-// // // // // //
-
-// Дана строка. Необходимо вывести в консоль перевёрнутый вариант. Например, "Hello" -> "olleH".
-
-const exampleStr = 'I can\'t stand redoing it';
-
-console.log(exampleStr.split('').reverse().join(''));
